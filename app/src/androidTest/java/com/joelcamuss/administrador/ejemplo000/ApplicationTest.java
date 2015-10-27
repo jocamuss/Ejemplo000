0e8d0b585a893cdd2a23d150c4cb9ea68df303e7
administrador <joel.camuss@gmail.com>
2015-10-27 08:59:49 +0100

    prueba de modificacion por joel camus

--- app/src/androidTest/java/com/joelcamuss/administrador/ejemplo000/ApplicationTest.java
+++ app/src/androidTest/java/com/joelcamuss/administrador/ejemplo000/ApplicationTest.java
@@ -10,4 +10,5 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+    //ejemplo de subir cambio en git 
 }
